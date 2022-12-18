fb5054e4c2d6a68909df0909f352cf00d6c69662
Manju31051996 <bbelagavi6@gmail.com>
2022-12-18 18:38:38 +0530

    [Manjunath S B] Day 14->Added The Test Case and Impl the HashMap Using LinkedList

--- src/com/bridgelabz/MyLinkedList.java
+++ src/com/bridgelabz/MyLinkedList.java
@@ -1,6 +1,6 @@
 package com.bridgelabz;
 
-public class MyLinkedList {
+public class MyLinkedList<K> {
     public INode tail;
     public INode head;
 
@@ -9,23 +9,23 @@ public class MyLinkedList {
         this.tail = null;
     }
 
-    public  void add(INode newNode) {
-        if (this.tail == null){
+    public void add(INode newNode) {
+        if (this.tail == null) {
             this.tail = newNode;
         }
-        if (this.head == null){
+        if (this.head == null) {
             this.head = newNode;
-        }else {
+        } else {
             INode tempNode = this.head;
             this.head = newNode;
             this.head.setNext(tempNode);
         }
     }
 
-    public  void printMyNode(){
+    public void printMyNode() {
         StringBuffer myNodes = new StringBuffer("My Nodes: ");
         INode tempNode = head;
-        while (tempNode.getNext() != null){
+        while (tempNode.getNext() != null) {
             myNodes.append(tempNode.getKey());
             if (!tempNode.equals(tail)) myNodes.append("->");
             tempNode = tempNode.getNext();
@@ -35,20 +35,19 @@ public class MyLinkedList {
     }
 
     public void append(INode myNode) {
-        if (this.head == null){
-            this.head =myNode;
+        if (this.head == null) {
+            this.head = myNode;
         }
-        if (this.tail == null){
+        if (this.tail == null) {
             this.tail = myNode;
-        }
-        else {
+        } else {
             this.tail.setNext(myNode);
             this.tail = myNode;
         }
     }
 
 
-    public void insert(INode myFirstNode,INode mySecondNode) {
+    public void insert(INode myFirstNode, INode mySecondNode) {
         INode tempnode = myFirstNode.getNext();
         myFirstNode.setNext(mySecondNode);
         mySecondNode.setNext(tempnode);
@@ -57,16 +56,37 @@ public class MyLinkedList {
     public INode popFirst() {
         INode tempNode = this.head;
         this.head = head.getNext();
-        return  tempNode;
+        return tempNode;
     }
 
     public INode popLast() {
         INode tempNode = head;
-        while (!tempNode.getNext().equals(tail)){
+        while (!tempNode.getNext().equals(tail)) {
             tempNode = tempNode.getNext();
         }
         this.tail = tempNode;
         tempNode.setNext(null);
         return tempNode;
+    }
+
+    @Override
+    public String toString() {
+        return "MyLinkedList{" +
+                "head=" + head +
+                '}';
+    }
+
+    public <K> INode search(K key) {
+        INode tempNode = head;
+        while (tempNode != null && tempNode.getNext() != null) {
+            if (tempNode.getKey().equals(key)) {
+                return tempNode;
+            }
+            tempNode = tempNode.getNext();
+        }
+        return null;
+    }
+    public  void  printMyNodes(){
+        System.out.println("My Nodes :"+head);
     }
 }
--- test/com/bridgelabz/MyLinkedListTest.java
+++ test/com/bridgelabz/MyLinkedListTest.java
@@ -91,4 +91,23 @@ public class MyLinkedListTest {
 
     }
 
+    @Test
+    public void searchGivenElementWhenFoundShouldPssLinkedListResult() {
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(myFirstNode);
+        myLinkedList.append(mySecondNode);
+        myLinkedList.append(myThirdNode);
+
+        myLinkedList.search(mySecondNode);
+        myLinkedList.printMyNode();
+        boolean result = myLinkedList.head.equals(myFirstNode) &&
+                myLinkedList.head.getNext().equals(mySecondNode) &&
+                myLinkedList.tail.equals(mySecondNode);
+        Assert.assertTrue(result);
+
+    }
 }
+
